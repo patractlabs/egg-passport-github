92e013735213e2d67351f98dd76f437c9c00b866
Zhichao Zhang <zzcwoshizz@163.com>
2021-11-11 11:18:35 +0800

    replace user struct

--- app.js
+++ app.js
@@ -11,20 +11,16 @@ module.exports = app => {
   assert(config.secret, '[egg-passport-github] config.passportGithub.secret required');
   config.clientID = config.key;
   config.clientSecret = config.secret;
+  config.scope = ['email']
 
   // must require `req` params
   app.passport.use('github', new Strategy(config, (req, accessToken, refreshToken, params, profile, done) => {
     // format user
     const user = {
-      provider: 'github',
-      id: profile.id,
-      name: profile.username,
-      displayName: profile.displayName,
-      photo: profile.photos && profile.photos[0] && profile.photos[0].value,
-      accessToken,
-      refreshToken,
-      params,
-      profile,
+      login: profile.username,
+      name: profile.displayName || profile.username,
+      workid: profile.id,
+      email: (profile.emails && profile.emails[0] && profile.emails[0].value) || '',
     };
 
     debug('%s %s get user: %j', req.method, req.url, user);
